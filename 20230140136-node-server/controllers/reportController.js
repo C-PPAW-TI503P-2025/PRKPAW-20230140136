e60c9983b9addf525f9d3aae892b5d9a7f07e392
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-04 19:03:04 +0700

    tugas5 Melengkapi RESTful API

--- 20230140136-node-server/controllers/reportController.js
+++ 20230140136-node-server/controllers/reportController.js
@@ -1,9 +1,35 @@
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
 const presensiRecords = require("../data/presensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    if (tanggalMulai && tanggalSelesai) {
+      options.where.tanggalPresensi = {
+        [Op.between]: [new Date(tanggalMulai), new Date(tanggalSelesai)],
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
+  }
 };
+
 
--- 20230140136-node-server/middleware/permissionMiddleware.js
+++ 20230140136-node-server/middleware/permissionMiddleware.js
@@ -1,9 +1,9 @@
  	exports.addUserData = (req, res, next) => {
  	  console.log('Middleware: Menambahkan data user dummy...');
  	  req.user = {
- 	    id: 123,
- 	    nama: 'User admin1',
- 	    role: 'admin'
+ 	    id: 125,
+ 	    nama: 'User 1',
+ 	    role: 'karyawan'
  	  };
  	  next(); 
  	};
@@ -17,3 +17,24 @@
  	    return res.status(403).json({ message: 'Akses ditolak: Hanya untuk admin'});
  	  }
  	};
+
+	const { body, validationResult } = require('express-validator');
+
+// Middleware validasi untuk Presensi (checkIn & checkOut)
+exports.validatePresensi = [
+  body('checkIn')
+    .optional()
+    .isISO8601()
+    .withMessage('Format checkIn tidak valid, gunakan format tanggal ISO8601'),
+  body('checkOut')
+    .optional()
+    .isISO8601()
+    .withMessage('Format checkOut tidak valid, gunakan format tanggal ISO8601'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
